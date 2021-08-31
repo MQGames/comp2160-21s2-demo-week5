2c6b7c159d0b1d23431f96d600c43c6bf60f3bf7
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2021-08-31 10:31:11 +1000

    Added a cooldown to the firing script

--- COMP2160 Demo Week 5/Assets/Scripts/ShootBullet.cs
+++ COMP2160 Demo Week 5/Assets/Scripts/ShootBullet.cs
@@ -6,17 +6,27 @@ public class ShootBullet : MonoBehaviour
 {
     [SerializeField]
     private Bullet bulletPrefab;
-    
+    [SerializeField]
+    private float cooldown = 0.5f; 
+    private float timer = 0;
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 pos = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(pos);
+        timer -= Time.deltaTime;
 
-            Bullet b = Instantiate(bulletPrefab);
-            b.transform.position = ray.origin;
-            b.Direction = ray.direction;
+        if (timer <= 0) 
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 pos = Input.mousePosition;
+                Ray ray = Camera.main.ScreenPointToRay(pos);
+
+                Bullet b = Instantiate(bulletPrefab);
+                b.transform.position = ray.origin;
+                b.Direction = ray.direction;
+                timer = cooldown;
+            }
         }
+
     }
 }
